aa5705f8fca1797e5663d80584a0a5e4cb55dbfb
Jacek Rojek <j.rojek@gmail.com>
2022-11-09 17:06:02 +0100

    add scene

--- src/RoomModel.js
+++ src/RoomModel.js
@@ -1,12 +1,15 @@
 import { useLoader } from "@react-three/fiber";
 import { Suspense } from "react";
+import room from "./assets/room.gltf";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function RoomModel() {
-  const gltf = useLoader(GLTFLoader, "../assets/room.gltf");
+  const gltf = useLoader(GLTFLoader, room);
   return (
     <Suspense fallback={null}>
-      <primitive object={gltf.scene} />
+      <group position={[-1, -1, 5]}>
+        <primitive object={gltf.scene} />
+      </group>
     </Suspense>
   );
 }
